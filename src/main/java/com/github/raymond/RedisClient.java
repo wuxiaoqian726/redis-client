c3c20f3cee2a2c167a8f2164a24ea191e43dc1f0
RWu <raymond.wu@suryani.cn>
2013-11-01 17:45:29 +0800

    design the main class of the redis client

--- src/main/java/com/github/raymond/RedisClient.java
+++ src/main/java/com/github/raymond/RedisClient.java
@@ -4,4 +4,20 @@ package com.github.raymond;
  * @author raymond
  */
 public class RedisClient {
+    private final String host;
+    private final int port;
+
+    public RedisClient(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public void set(String key, String value) {
+
+    }
+
+    public void get(String key) {
+
+    }
+
 }
